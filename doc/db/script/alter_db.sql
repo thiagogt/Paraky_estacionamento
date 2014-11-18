d068306758bd24f56153df4fa91cc34d9d8a8199
Thiago Gomes Toledo <thimakgt@gmail.com>
2014-11-18 11:10:11 -0200

    Arrumando os scripts do bd

--- doc/db/script/alter_db.sql
+++ doc/db/script/alter_db.sql
@@ -3,4 +3,4 @@ ALTER TABLE car ADD CONSTRAINT fk_car_client FOREIGN KEY (id_client_car) REFEREN
 ALTER TABLE daily_payment ADD CONSTRAINT fk_car_charged FOREIGN KEY (id_car_charged) REFERENCES car (id_car) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE parking_space ADD CONSTRAINT fk_client_owner_space FOREIGN KEY (id_client_owner) REFERENCES client (id_client) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE monthly_payment ADD CONSTRAINT fk_payment_space FOREIGN KEY (id_parking_space) REFERENCES parking_space (id_parking_space) ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE adress ADD CONSTRAINT fk_adress_client FOREIGN KEY (id_adress_client) REFERENCES client (id_client) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE adress ADD CONSTRAINT fk_adress_client FOREIGN KEY (id_client_adress) REFERENCES client (id_client) ON UPDATE CASCADE ON DELETE CASCADE;
--- doc/db/script/populate.sql
+++ doc/db/script/populate.sql
@@ -1,7 +1,7 @@
 
 INSERT INTO version_bd ( id , version) values(1,'1.0');
 
-INSERT INTO client (id_client,name,last_name,cpf,birthday_date,tel_1 ,tel_2 ,email,id_owner_parking_space ) values (1,'usuarioTeste','primeiro','123456789','2008-08-22 00:00:00.000000000','12345-555','1234-555','teste@mail.com',0);
+INSERT INTO client (id_client,name,last_name,cpf,birthday_date,tel_1 ,tel_2 ,email,id_owner_parking_space ) values (1,'usuarioTeste','primeiro','123456789','2008-08-22 00:00:00.000000000','12345-555','1234-555','teste@mail.com',null);
 INSERT INTO client (id_client,name,last_name,cpf,birthday_date,tel_1 ,tel_2 ,email,id_owner_parking_space ) values (2,'usuarioTeste','segundo','123456780','2008-08-22 00:00:00.000000000','12345-555','1234-555','teste@mail.com',1);
 
 INSERT INTO car (id_car ,id_client_car,year_manufacture ,color,model,car_plate,car_brand) values (1,1,2000,'green','pegoult 1.8','LCY1884','Pegoult');
