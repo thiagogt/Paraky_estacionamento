82dbb85b797c309f253b69fb3aff98c644135ad2
Thiago Gomes Toledo <thimakgt@gmail.com>
2014-12-11 21:53:51 -0200

    continuando metodo thread day

--- src/br/com/parakyestacionamento/thread/VerifyActualDayThread.java
+++ src/br/com/parakyestacionamento/thread/VerifyActualDayThread.java
@@ -1,6 +1,14 @@
 package br.com.parakyestacionamento.thread;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import org.joda.time.DateTime;
+
+import br.com.parakyestacionamento.dominio.Client;
+import br.com.parakyestacionamento.dominio.MonthlyPaymentPerClient;
+import br.com.parakyestacionamento.modeloBD.ClientModelBD;
+import br.com.parakyestacionamento.modeloBD.MonthlyPaymentPerClientModelBD;
 
 
 public class VerifyActualDayThread extends Thread{
@@ -22,6 +30,33 @@ public class VerifyActualDayThread extends Thread{
 	}
 
 	private void createMonthlyPayment(int actualDay) {
+		List<MonthlyPaymentPerClient> monthlyPerClientList = null;
+		List<Client> clientList = null;
+		
+		MonthlyPaymentPerClientModelBD modelMonthlyPerClient = new MonthlyPaymentPerClientModelBD();
+		ClientModelBD modelClient = new ClientModelBD();
+		
+		try {
+			
+			clientList = modelClient.selectAllOwners();
+			monthlyPerClientList = modelMonthlyPerClient.selectAllDebtPerClient();
+			
+			for (Client client : clientList) {
+				
+				if(todayIsPayDay(client));
+					if(thisClientWasNotAlreadyChargedToday(client))
+			}
+			
+			
+			
+			
+			
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
